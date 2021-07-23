e9aa60a28b9ec999ebdd3bec749ef97ded310a56
hp <princee733@gmail.com>
2021-07-23 16:16:49 +0530

    first cmmit

---
+++ accounts/urls.py
@@ -0,0 +1,19 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    
+    path('register/', views.registerPage, name = "register"),
+    path('login/', views.loginPage, name = "login"),
+    path('logout/', views.logoutUser, name = "logout"),
+
+
+    path('customer/<str:pk>', views.customer, name="customer"),
+    path('products/', views.products, name="products"),
+    path('', views.home, name="home"), 
+
+    path('create_order/', views.createOrder, name="create_order"),
+    path('update_order/<str:pk>', views.updateOrder, name="update_order"),
+    path('delete_order/<str:pk>', views.deleteOrder, name="delete_order")
+]
